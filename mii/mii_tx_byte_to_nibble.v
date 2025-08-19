55a53b842f13a5185538bfcd2b2eb7bccc49e5a7
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-08-19 09:55:22 -0400

    Create mii_tx_byte_to_nibble.v

---
+++ mii/mii_tx_byte_to_nibble.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/19/2025 05:34:38 AM
+// Design Name: 
+// Module Name: mii_tx_byte_to_nibble
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mii_tx_byte_to_nibble(
+    input wire  mii_tx_clk, // 25 MHz
+    input wire [7:0] tx_data,
+    input wire       tx_valid,
+    
+    output reg [3:0] mii_txd,
+    output reg       mii_tx_en
+
+    );
+    
+    reg [7:0] tx_buf;
+    reg       phase; // 0 = low nibble, 1 = high nibble
+    
+    always @(posedge mii_tx_clk) begin
+        if (!tx_valid) begin
+            mii_tx_en <= 0;
+            phase <= 0;
+            mii_txd <= 4'h0;
+            tx_buf <= 8'h00;
+         end else begin 
+            mii_tx_en <= 1;
+            if (!phase) begin
+                tx_buf <= tx_data;
+                // Reverse these two if data is backwards in hardware
+                // Assumption the second byte is the high 4 bits
+                mii_txd <= tx_data[3:0]; // low nibble first
+                phase <= 1;
+              end else begin
+                mii_txd <= tx_buf[7:4]; // high nibble second
+                phase <= 0;
+             end
+          end
+       end
+          
+              
+            
+endmodule
