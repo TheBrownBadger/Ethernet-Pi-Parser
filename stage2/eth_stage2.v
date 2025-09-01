4794d0e2edb13a9975c4f9c11a62ef8c22ef8f82
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-09-01 05:56:52 -0400

    Update eth_stage2.v

--- stage2/eth_stage2.v
+++ stage2/eth_stage2.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/18/2025 06:52:50 AM
-// Design Name: 
-// Module Name: eth_stage2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module eth_stage2(
