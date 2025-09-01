fc7386b95da88c25a519ba311272e80b7b229d96
Dajuan <107205602+TheBrownBadger@users.noreply.github.com>
2025-09-01 05:58:46 -0400

    Update eth_stage3.v

--- stage3/eth_stage3.v
+++ stage3/eth_stage3.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/18/2025 09:11:41 AM
-// Design Name: 
-// Module Name: eth_stage3
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 // Stage 3 Parser: Ethernet + IPv$ + TCP/UDP
 // Captures:
